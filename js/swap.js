d8ca82a61ff9ae17ed0f99c5a4438c3b7a825266
maximedaraize <maxime.daraize@gmail.com>
2020-04-05 10:41:46 -0400

    add condition when swapping stylesheet that add css class on pokemon container if its a bigger pokemon and remove it if its not

--- js/swap.js
+++ js/swap.js
@@ -1,6 +1,11 @@
 let styleSheet = document.querySelector("#stylesheet");
 let pokemonList = document.querySelector("#pokemonList");
 
-window.addEventListener("click", function() {
+window.addEventListener("click", function () {
   styleSheet.href = "css/" + pokemonList.value;
+  if (pokemonList.value == "056-mankey.css") {
+    document.querySelector(".canvas").classList.add("big");
+  } else {
+    document.querySelector(".canvas").classList.remove("big");
+  }
 });
